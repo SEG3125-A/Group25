d64a4d1c5fc1324febdad2b4979db7d9e3737a87
damien <damiencs00@gmail.com>
2024-02-08 18:01:28 -0500

    Added validatForm to button

--- lab4/alert.js
+++ lab4/alert.js
@@ -1,15 +1,15 @@
-function alert() {
-    if (document.forms['form'].emailAddress.value === "") {
-        alert("Email Address required!");
-        return false;
-    }
-    if (document.forms['form'].phoneNumber.value === "") {
-        alert("Phone number Required!");
-        return false;
-    }
-    if (document.forms['form'].dateTimeInput.value === "") {
-        alert("Date and Time Required!");
-        return false;
-    }
-    return true;
+function validateForm() {
+  if (document.forms["form"].emailAddress.value === "") {
+    alert("Email Address required!");
+    return false;
+  }
+  if (document.forms["form"].phoneNumber.value === "") {
+    alert("Phone number Required!");
+    return false;
+  }
+  if (document.forms["form"].dateTimeInput.value === "") {
+    alert("Date and Time Required!");
+    return false;
+  }
+  return true;
 }
