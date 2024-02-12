83beae0a517748b3a2b231d7e10380ae400183b3
Ayoub-Ell <aelai054@uottawa.ca>
2024-02-12 14:44:12 -0500

    Implemented phone number constraint, and input field hover/click

--- lab5/alert.js
+++ lab5/alert.js
@@ -7,9 +7,19 @@ function validateForm() {
     alert("Phone number Required!");
     return false;
   }
+  if (!isValid(document.forms["form"].phoneNumber.value)) {
+    alert("Phone number invalid!");
+    return false;
+  }
   if (document.forms["form"].dateTimeInput.value === "") {
     alert("Date and Time Required!");
     return false;
   }
   return true;
+}
+
+function isValid(p) {
+  var phoneRe = /^[1-9]\d{2}[1-9]\d{2}\d{4}$/;
+  var digits = p.toString().replace(/\D/g, "");
+  return phoneRe.test(digits);
 }
