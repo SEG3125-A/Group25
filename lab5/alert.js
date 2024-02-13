0130eb5473c768396bca1f62980958a16067f69e
shivanpillay20 <91153735+shivanpillay20@users.noreply.github.com>
2024-02-13 05:35:10 +0400

    jquery tooltip

--- lab5/alert.js
+++ lab5/alert.js
@@ -1,25 +1,70 @@
 function validateForm() {
-  if (document.forms["form"].emailAddress.value === "") {
-    alert("Email Address required!");
-    return false;
+  var cardholderName = document.forms["form"].cardholderName.value;
+  var phoneNumber = document.forms["form"].phoneNumber.value;
+  var dateTimeInput = document.forms["form"].dateTimeInput.value;
+
+  if (!isValidName(cardholderName)) {
+      alert("Invalid Name! Please enter a valid name without numbers.");
+      return false;
   }
-  if (document.forms["form"].phoneNumber.value === "") {
-    alert("Phone number Required!");
-    return false;
+
+  if (phoneNumber === "") {
+      alert("Phone number Required!");
+      return false;
   }
-  if (!isValid(document.forms["form"].phoneNumber.value)) {
-    alert("Phone number invalid!");
-    return false;
+  if (!isValid(phoneNumber)) {
+      alert("Phone number invalid!");
+      return false;
   }
-  if (document.forms["form"].dateTimeInput.value === "") {
-    alert("Date and Time Required!");
-    return false;
+  if (dateTimeInput === "") {
+      alert("Date and Time Required!");
+      return false;
   }
   return true;
 }
+
+function isValidName(name) {
+
+  var nameRe = /\d/;
+  return !nameRe.test(name);
+}
+
 
 function isValid(p) {
   var phoneRe = /^[1-9]\d{2}[1-9]\d{2}\d{4}$/;
   var digits = p.toString().replace(/\D/g, "");
   return phoneRe.test(digits);
 }
+
+
+
+
+$(document).ready(function () {
+  // Tooltip for the "CARDHOLDER NAME" input
+  $("#cardholderName").tooltip({
+      classes: {
+          "ui-tooltip": "highlight"
+      },
+      track: true
+  });
+});
+$(document).ready(function () {
+  // Tooltip for the "CARDHOLDER NAME" input
+  $("#cvv").tooltip({
+      classes: {
+          "ui-tooltip": "highlight"
+      },
+      track: true
+  });
+});
+$(document).ready(function () {
+  // Tooltip for the "CARDHOLDER NAME" input
+  $("#cardnumber").tooltip({
+      classes: {
+          "ui-tooltip": "highlight"
+      },
+      track: true
+  });
+});
+
+
